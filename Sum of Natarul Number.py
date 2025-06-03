794c656e830c7f96acfb867ce864c3f9b36c772e
Tahir Khan <tktahirkhan2010@gmail.com>
2025-06-03 22:26:22 +0600

    Add files via upload

---
+++ Sum of Natarul Number.py
@@ -0,0 +1,8 @@
+# input the value of ters
+n = int(input("Enter the vlue of terms: "))
+sum = 0 #initialize
+i = 1 #initialize
+while i <= n: #loop will run from 1 to n
+    sum = sum+i
+    i = i + 1
+print("\nSum=", sum)
